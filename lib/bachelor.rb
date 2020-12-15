3700fb3d0eb84d9f2952898c8fe898ae6747a5fa
Raz Al-jaf <razaljaf@gmail.com>
2020-12-15 16:32:51 -0700

    commit tree 31a679ef0a19b914584a4ab18fc989c5e6bd7ca4

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,19 @@
+
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = data[season].find do |contestant|
+    contestant['status'] == "Winner"
+  end
+  winner_name = winner['name']
+  winner_name.split(" ")[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  occupation = data.map do |contestant|
+    if occupation == contestant[occupation]
+      return contestant[name]
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
